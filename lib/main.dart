adaab400c25afb8ce911511295d8be293d7943e5
Ajeem Ahmad <ahmedazim7804@gmail.com>
2024-10-23 22:30:28 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tictactoe/startscreen.dart';
+import 'package:tictactoe/game_screen.dart';
+
+void main() => runApp(MaterialApp(home: StartScreen()));
